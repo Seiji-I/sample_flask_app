c5cd7a45b62a622c535bc10f6557f07ebc32718f
S-141 <s3bxed63p@gmail.com>
2022-09-10 17:04:59 +0900

    commit

--- flask/app.py
+++ flask/app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def index():
   return "<h1>index page</h1>"
 
+@app.route("/hello")
+def hello():
+  return "<h1>Hello</h1>"
+
+@app.route("/goodbye")
+def goodbye():
+  return "<h1>Good Bye!</h1>"
+
+@app.route("/hi")
+def hi():
+  return "<h1>Hi!</h1>"
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5555)
